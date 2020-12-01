4de249e43697394b43b49732d59ca2d4016c55f3
Mitra Shah <mitrashah94@gmail.com>
2020-12-01 20:38:26 +0000

    Drive Bot Node
    - Clean drive_bot node of ball_chaser package

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/workspace/Go_chase_it_RSEND/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/workspace/Go_chase_it_RSEND/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/workspace/Go_chase_it_RSEND/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -10,38 +10,39 @@ ros::Publisher motor_command_publisher;
 // After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
 bool handle_drive_request (ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res)
 {
-    ROS_INFO("Service request : DriveToTarget recieved. Go!");
-    geometry_msgs::Twist motor_command; //motor_command object of type geometry_msgs::Twist
-        
-    motor_command.linear.x = req.linear_x; //Publish angles to drive the robot
-    motor_command.angular.z = req.angular_z;
+  auto linear_x = (float) req.linear_x;
+  auto angular_z = (float) req.angular_z;
+  
+  ROS_INFO("Service request : DriveToTarget recieved. Start!");
+  geometry_msgs::Twist motor_command; //motor_command object of type geometry_msgs::Twist
 
-    motor_command_publisher.publish(motor_command);
+  motor_command.linear.x = linear_x; //Publish angles to drive the robot
+  motor_command.angular.z = angular_z;
 
-    res.msg_feedback = "velocity_linear_x: " + std::to_string(req.linear_x) + ", velocity_angular_z: " + std::to_string(req.angular_z);
-    ROS_INFO_STREAM(res.msg_feedback); //Response
+  motor_command_publisher.publish(motor_command);
 
-    return true;
+  res.msg_feedback = "velocity_linear_x: " + std::to_string(linear_x) + ", velocity_angular_z: " + std::to_string(angular_z);
+  ROS_INFO_STREAM(res.msg_feedback); //Response
+
+  return true;
 }
-
-
 
 int main(int argc, char** argv)
 {
-    // Initialize a ROS node
-    ros::init(argc, argv, "drive_bot");
+  // Initialize a ROS node
+  ros::init(argc, argv, "drive_bot");
 
-    // Create a ROS NodeHandle object
-    ros::NodeHandle n;
+  // Create a ROS NodeHandle object
+  ros::NodeHandle n;
 
-    // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
-    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
+  motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-    // TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
-    ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
+  // TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
+  ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
 
-    // TODO: Handle ROS communication events
-    ros::spin();
+  // TODO: Handle ROS communication events
+  ros::spin();
 
-    return 0;
+  return 0;
 }
